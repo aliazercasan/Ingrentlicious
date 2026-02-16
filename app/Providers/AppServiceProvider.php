87e29217505fd695b7a74fc35f8bd5d738f43aa8
Aliazer Solaiman <solaimanaliazermain@gmail.com>
2026-02-16 11:06:04 +0800

    Add documentation for fixing Railway URL issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -33,11 +33,6 @@ class AppServiceProvider extends ServiceProvider
             if ($appUrl = config('app.url')) {
                 \Illuminate\Support\Facades\URL::forceRootUrl($appUrl);
             }
-            
-            // Ensure secure cookies in production
-            config(['session.secure' => true]);
-            config(['session.http_only' => true]);
-            config(['session.same_site' => 'lax']);
         }
     }
 
